213694d09fb91309f6c9a83adaed09c8bf84063e
Peter Wilms <peterwilms@hotmail.de>
2017-06-24 02:21:43 +0200

    initial commit

---
+++ ApiRequests.js
@@ -0,0 +1,49 @@
+const https = require("https");
+
+
+const postRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            "method": "POST",
+            "hostname": "www.googleapis.com",
+            "port": null,
+            "path": `/urlshortener/v1/url?key=${process.env.APIKEY}`,
+            "headers": {
+                "content-type": "application/json",
+            }
+        };
+
+        let request = https.request(options, (response) => {
+            let chunks = [];
+            response.on("data", (chunk) => {
+                chunks.push(chunk);
+            });
+            response.on("error", (err) => {
+                reject(new Error(err));
+            })
+            response.on("end", () => {
+                let body = Buffer.concat(chunks);
+                resolve(body.toString());
+            })
+
+        })
+
+        request.write(JSON.stringify({ longUrl: 'https://nodejs.org/api/http.html#http_http_request_options_callback' }));
+        request.end()
+
+
+
+    })
+}
+
+module.exports = {
+    POST: async function(url) {
+        try {
+            let request = await postRequest(url)
+            return request;
+        } catch (err) {
+            throw err
+        }
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const app = require("express")();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 1000;
+const { POST } = require("./ApiRequests");
+
+
+app.get("/", (request, response) => {
+    console.log(request.query.url)
+    if (request.query.url !== undefined) {
+        POST(request.query.url)
+            .then((data) => {
+                console.log(data)
+                return response.send(data)
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+    } else response.send("Error: No query given!")
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
